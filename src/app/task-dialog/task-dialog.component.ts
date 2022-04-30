123f586369a8643737e290f4c14a92dbcf8f2b42
Rodion Sibov <rodionsibov@gmail.com>
2022-04-30 09:26:23 +0300

    first commit

---
+++ src/app/task-dialog/task-dialog.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { TaskInterface } from '../task/task-interface';
+
+@Component({
+  selector: 'app-task-dialog',
+  templateUrl: './task-dialog.component.html',
+  styleUrls: ['./task-dialog.component.css'],
+})
+export class TaskDialogComponent implements OnInit {
+  private backupTask: Partial<TaskInterface> = { ...this.data.task };
+
+  constructor(
+    public dialogRef: MatDialogRef<TaskDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: TaskDialogData
+  ) {}
+
+  ngOnInit(): void {}
+
+  cancel(): void {
+    this.data.task.title = this.backupTask.title;
+    this.data.task.description = this.backupTask.description;
+    this.dialogRef.close(this.data);
+  }
+}
+
+export interface TaskDialogData {
+  task: Partial<TaskInterface>;
+  enableDelete: boolean;
+}
+
+export interface TaskDialogResult {
+  task: TaskInterface;
+  delete?: boolean;
+}
